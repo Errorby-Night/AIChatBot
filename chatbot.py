9a5633d4749df078a160e3787f00a69b3f00ec48
Srijan <srijandutta200@gmail.com>
2023-02-14 12:13:05 +0530

    new approach

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,6 @@
-
+import requests
+import os
+import openai
 
 class ChatBot:
 
@@ -15,7 +17,26 @@ class ChatBot:
  #####  # #####  #####  ###### #    #  #####  #    # #####  ###### #    # """
         print(self.art)
         print(f"\nAuthor:{self.author()}")
+        print("Enter your query or type 'exit' to exit.")
         print("\n\nWelcome!\nHow can I help you today?")
+
 
     def author(self):
         return "Srijan Dutta"
+
+    def bot(self, message):
+        openai.api_key = os.getenv("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")
+        response = openai.Completion.create(model="text-davinci-003", prompt=message, temperature=1, max_tokens=15)
+        return response
+    
+    def keep_running(self):
+        message = input("==>")
+        while not(message.lower() == "exit"):
+            output = self.bot(message)
+            print(output)
+            message = input("==>")
+        
+if __name__ == "__main__":
+    bot = ChatBot()
+    bot.welcomemessage()
+    bot.keep_running()
